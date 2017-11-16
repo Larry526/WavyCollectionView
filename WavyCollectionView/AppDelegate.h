c1220f927ee366cd1c425ba26bc80cac3d02f605
Larry Luk <larryluk526@gmail.com>
2017-11-16 10:43:37 -0500

    Initial Commit

---
+++ WavyCollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WavyCollectionView
+//
+//  Created by Larry Luk on 2017-11-16.
+//  Copyright © 2017 Larry Luk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WavyCollectionViewTests/WavyCollectionViewTests.m
@@ -0,0 +1,39 @@
+//
+//  WavyCollectionViewTests.m
+//  WavyCollectionViewTests
+//
+//  Created by Larry Luk on 2017-11-16.
+//  Copyright © 2017 Larry Luk. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WavyCollectionViewTests : XCTestCase
+
+@end
+
+@implementation WavyCollectionViewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
